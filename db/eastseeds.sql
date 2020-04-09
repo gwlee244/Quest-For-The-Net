1a106c742730be8478736aa55ab38f93dbbac5e3
Nicholas Finch <nicfinch223@gmail.com>
2020-04-09 19:33:09 -0400

    East coast seed

---
+++ db/eastseeds.sql
@@ -0,0 +1,152 @@
+SELECT * FROM sequelize_basketball.players;
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Kemba', 'Walker', 'Point Guard', 25.6, 43.4, 35.6, 84.4, 4.4, 5.9, 1.2, 0.4, 88);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Kyrie', 'Irving', 'Point Guard', 23.8, 48.7, 40.1, 87.3, 5.0, 6.9, 1.5, 0.5, 92);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Zach', 'Lavine', 'Point Guard', 23.7, 46.7, 37.4, 83.2, 4.7, 4.5, 1.0, 0.4, 86);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Trae', 'Young', 'Point Guard', 19.1, 41.8, 32.4, 82.8, 3.7, 8.1, 0.9, 0.2, 84);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Ben', 'Simmons', 'Point Guard', 16.9, 56.3, 0.0, 60.0, 8.8, 7.7, 1.4, 0.8, 88);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Jordan', 'Clarkson', 'Point Guard', 16.8, 44.8, 32.4, 84.4, 3.3, 2.4, 0.7, 0.2, 79);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Spencer', 'Dinwiddie', 'Point Guard', 16.8, 44.2, 33.5, 80.6, 2.4, 4.6, 0.6, 0.3, 78);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Collin', 'Sexton', 'Point Guard', 16.7, 43.0, 40.2, 83.9, 2.9, 3.0, 0.5, 0.1, 77);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Eric', 'Bledsoe', 'Point Guard', 15.9, 48.4, 32.9, 75.0, 4.6, 5.5, 1.5, 0.4, 85);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Malcolm', 'Brogden', 'Point Guard', 15.6, 50.5, 42.6, 92.8, 4.5, 3.2, 0.7, 0.2, 83);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Bradley', 'Beal', 'Shooting Guard', 25.6, 47.5, 35.1, 80.8, 5.0, 5.5, 1.5, 0.7, 88);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ("D'angelo", 'Russell', 'Shooting Guard', 21.1, 43.4, 36.9, 78.0, 3.9, 7.0, 1.2, 0.2, 87);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('J.J', 'Reddick', 'Shooting Guard', 18.1, 44.0, 39.7, 89.4, 2.4, 2.7, 0.4, 0.2, 80);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Josh', 'Richardson', 'Shooting Guard', 16.6, 41.2, 35.7, 86.1, 3.6, 4.1, 1.1, 0.5, 80);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Jeremy', 'Lamb', 'Shooting Guard', 15.3, 44.0, 34.8, 88.8, 5.5, 2.2, 1.1, 0.4, 79);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Evan', 'Fournier', 'Shooting Guard', 15.1, 43.8, 34.0, 80.6, 3.2, 3.6, 0.9, 0.1, 77);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Terrence', 'Ross', 'Shooting Guard', 15.1, 42.8, 38.3, 87.5, 3.5, 1.7, 0.9, 0.4, 77);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Dwayne', 'Wade', 'Shooting Guard', 15.0, 43.3, 33.0, 70.8, 4.0, 4.2, 0.8, 0.5, 81);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Jaylen', 'Brown', 'Shooting Guard', 13.0, 46.5, 34.4, 65.8, 4.2, 1.4, 0.9, 0.4, 79);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Kent', 'Bazemore', 'Shooting Guard', 11.6, 40.2, 32.0, 72.6, 3.9, 2.3, 1.3, 0.6, 76);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Kawhi', 'Leonard', 'Small Forward', 26.6, 49.6, 37.1, 85.4, 7.3, 3.3, 1.8, 0.4, 95);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Khris', 'Middleton', 'Small Forward', 18.3, 44.1, 37.8, 83.7, 6.0, 4.3, 1.0, 0.1, 83);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Bojan', 'Bogdanovic', 'Small Forward', 18.0, 49.7, 42.5, 80.7, 4.1, 2.0, 0.9, 0.0, 83);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Marcus', 'Morris', 'Small Forward', 13.9, 44.7, 37.5, 84.4, 6.1, 1.5, 0.6, 0.3, 79);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Joe', 'Harris', 'Small Forward', 13.7, 50.0, 47.4, 82.7, 3.8, 2.4, 0.5, 0.2, 78);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Cedi', 'Osman', 'Small Forward', 13.0, 42.7, 34.8, 77.9, 4.7, 2.6, 0.8, 0.1, 74);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Kevin', 'Knox', 'Small Forward', 12.8, 37.0, 34.3, 71.7, 4.5, 1.1, 0.6, 0.3, 74);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Justise', 'Winslow', 'Small Forward', 12.6, 43.3, 37.5, 62.8, 5.4, 4.3, 1.1, 0.3, 78);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Gordon', 'Hayward', 'Small Forward', 11.5, 46.6, 33.3, 83.4, 4.5, 3.4, 0.9, 0.3, 80);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Demarre', 'Carroll', 'Small Forward', 11.1, 39.5, 34.2, 76.0, 5.2, 1.3, 0.5, 0.1, 77);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Giannis', 'Antetokounmpo', 'Power Forward', 27.7, 57.8, 25.6, 72.9, 12.5, 5.9, 1.3, 1.5, 96);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Blake', 'Griffin', 'Power Forward', 24.5, 46.2, 36.0, 75.3, 7.5, 5.4, 0.7, 0.4, 87);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('John', 'Collins', 'Power Forward', 19.5, 56.0, 34.8, 76.3, 9.8, 2.0, 0.4, 0.6, 84);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Pascal', 'Siakam', 'Power Forward', 16.9, 574.9, 36.9, 78.5, 6.9, 3.1, 0.9, 0.7, 85);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Aaron', 'Gordon', 'Power Forward', 16.0, 44.9, 34.9, 73.1, 7.4, 3.7, 0.7, 0.7, 80);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Jayson', 'Tatum', 'Power Forward', 15.7, 45.0, 37.3, 85.5, 6.0, 2.1, 1.1, 0.7, 84);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks,overallstats )
+values ('Jabari', 'Parker', 'Power Forward', 14.5, 49.3, 31.3, 71.2, 6.6, 2.4, 0.7, 0.5, 79);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Domantas', 'Sabonis', 'Power Forward', 14.1, 59.0, 52.9, 71.5, 9.3, 2.9, 0.6, 0.4, 82);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Al', 'Horford', 'Power Forward', 13.6, 53.5, 36.0, 82.1, 6.7, 4.2, 0.9, 1.3, 86);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Thaddeus', 'Young', 'Power Forward', 12.6, 52.7, 34.9, 64.4, 6.5, 2.5, 1.5, 0.4, 79);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Joel', 'Embiid', 'Center', 27.5, 48.4, 30.0, 80.4, 13.6, 3.7, 0.7, 1.9, 93);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Nikola', 'Vucevic', 'Center', 20.8, 51.8, 36.4, 78.9, 12.0, 3.8, 1.0, 1.1, 87);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Andre', 'Drummond', 'Center', 17.3, 53.3, 13.2, 59.0, 15.6, 1.4, 1.7, 1.7, 87);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Serge', 'Ibaka', 'Center', 15.0, 52.9, 29.0, 76.3, 8.1, 1.3, 0.4, 1.4, 82);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Myles', 'Turner', 'Center', 13.3, 48.7, 38.8, 73.6, 7.2, 1.6, 0.8, 2.7, 82);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Brook', 'Lopez', 'Center', 12.5, 45.2, 36.5, 84.2, 4.9, 1.2, 0.6, 2.2, 79);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Hassan', 'Whiteside', 'Center', 12.3, 57.1, 12.5, 44.9, 11.3, 0.8, 0.6, 1.9, 83);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Alex', 'Len', 'Center', 11.1, 49.4, 36.3, 64.8, 5.5, 1.1, 0.4, 0.9, 77);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Jarrett', 'Allen', 'Center', 10.9, 59.0, 13.3, 70.9, 8.4, 1.4, 0.5, 1.5, 81);
+
+insert into players (firstname, lastname, positionname, pointspergame, fieldgoal, threepoint, freethrowpercent, rebounds, assists, steals, blocks, overallstats )
+values ('Dewayne', 'Dedmon', 'Center', 10.8, 49.2, 38.2, 81.4, 7.5, 1.4, 1.1, 1.1, 77);
+
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,7 +11,6 @@ module.exports = function(app) {
       res.render('players', {
         error: err
       });
-      
     });
 });
 }
