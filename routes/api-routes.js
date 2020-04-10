b2876f5e81e2dc9b2fbef4e6a33533895453fe2b
ARauf Git <umiasad2017@gmail.com>
2020-04-09 21:42:35 -0400

    updated player model

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,36 +15,5 @@ module.exports = function(app) {
     });
 });
 
-// POST route for creating a new game
-app.post("/api/games", function(req, res) {
-  db.Game.create({
-    date: req.body.date,
-    time: req.body.time,
-    location: req.body.location,
-    max_players: req.body.max_players
-  }).then(function() {
-    res.redirect("/games");
-  }).catch((err) => {
-    res.render('games', {
-      error: err
-    });
-    // Handle any error that occurred in any of the previous
-    // promises in the chain.
-  });
-});
 
-app.post("/api/games", function(req, res) {
-  db.UserGame.create({
-      GameId: req.params.GameId,
-      UserId: req.params.UserId
-  }).then(function(){
-      res.redirect("/games");
-  }).catch((err) => {
-      res.render('games', {
-        error: err
-      });
-      // Handle any error that occurred in any of the previous
-      // promises in the chain.
-    });
-});
 }
