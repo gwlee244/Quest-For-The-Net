35de1fd3e3080b6c2c503ec525af40fcbf6fcbc4
Christian Sanchez <cas81695@gmail.com>
2020-04-13 13:10:58 -0400

    commit cahanges from asad oush 4/13

--- public/js/players.js
+++ public/js/players.js
@@ -1,20 +1,112 @@
 
+    
+//     // GET route for registered players from users database
+//     $.get("/api/all", (data) => {
+//         if (data.length !== 0) {
+//             for (var i = 0; i < data.length; i++) {
+//             let card = $(`
+//             <div class="row d-flex player-div">
+
+//             <button type="submit" class="addbtn btn-danger center-block off-set-70" name="addTo" data-id="${data[i].id}">Add to Roster</button>
+
+//             <button type="submit" name="remove" class="removebtn btn-primary center-block off-set-70">Remove from Team</button>
+
+//             <button class="delete btn btn-danger" data-id="${data[i].id}">Delete</button>"
+            
+//                 <div class="col-6">
+//                 <div class="player-card">
+//                         <div class="player-card__side player-card__side--front">
+//                             <div class="player-card__picture player-card__picture--1">
+//                                 &nbsp;
+//                             </div>
+
+//                             <div class="player-card__details">
+//                             <span class="game-details game-details--name">${data[i].firstname} ${data[i].lastname} : ${data[i].positionname}</span>
+                            
+//                             </div>
+//                         </div>
+//                 <div class="col-6">
+//                         <div class="player-card__side player-card__side--back player-card__side--back-1">
+//                             <div class="player-card__cta">
+//                             <div class="card__price-box">
+//                             <span class="game-details game-details--stats">PTS: ${data[i].pointspergame} REB: ${data[i].rebounds} AST: ${data[i].assists} <br><br> </span>
+                            
+//                             <span class="game-details game-details--icon"><i class="fas fa-basketball-ball fa-4x icon-back"></i></span><p><br><button class="devour btn btn-warning btn-sm" 
+//                             data-burgerid="{{id}}">Add Player to Team</button></p>
+
+
+
+//                         </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     </div>
+//                     </div>
+//                 </div>
+//             `);
+//              $(".players-container").prepend(card);
+            
+//             }
+//         }
+//         });
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// $(".players-container").on("click", ".addbtn", function(event) {
+//     event.preventDefault();
+//     console.log("this was clicked");
+//     // let addplayer =  $(`
+//     // <p>something</p>`);
+//     let addplayer = $(event.target).closest(".player-div");
+//     const id = $(event.target).data("id");
+//     $.ajax({
+//         method: "PUT",
+//         url: "/api/players/" + id
+//       })
+//         .then(function(response) {
+//           $(".addedPlayers").append(addplayer);
+            
+//             console.log("response", response);
+//            //window.location.reload();
+//         });
+        
+// })
+
+
+        
+// ///////////////////////////////////////////////////////////////////////////////////////////////////   
+//         $(".players-container").on("click", ".delete", function(event) {
+//             event.preventDefault();
+//             console.log("delete button clicked");
+//             const id = $(event.target).data("id");
+//             $.ajax({
+//                 method: "DELETE",
+//                 url: "/api/players/" + id
+//               })
+//                 .done(function(response) {
+//                   //getPosts(postCategorySelect.val());
+//                     console.log("response", response);
+//                     location.reload();
+//                 });
+                
+//         })
+
 // GET route for registered players from users database
 $.get("/api/all", (data) => {
     if (data.length !== 0) {
         for (var i = 0; i < data.length; i++) {
         let card = $(`
-        <div class="player-div">
+        <div class="row player-div">
             <div class="col-md-6">
-            <button type="submit" class="addbtn  btn btn-primary data-id="${data[i].id}"><i class="fas fa-plus"></i></button>
-            <button type="submit" name="Remove" class="removebtn btn btn-danger  center-block off-set-70"><i class="fas fa-minus"></i></button>
+            <button type="submit" class="addbtn  center-block off-set-70" name="addTo" data-id="${data[i].id}">Add to Roster</button>
+            <button type="submit" name="remove" class="removebtn  center-block off-set-70">Remove from Team</button>
             <div class="player-card">
                     <div class="player-card__side player-card__side--front">
                         <div class="player-card__picture player-card__picture--1">
                             &nbsp;
                         </div>
                         <div class="player-card__details">
-                        <span class="game-details game-details--name">${data[i].firstname} ${data[i].lastname}</span>
+                        <span class="game-details game-details--name">${data[i].firstname} ${data[i].lastname} : ${data[i].positionname}</span>
                         </div>
                     </div>
                     
@@ -32,7 +124,7 @@ $.get("/api/all", (data) => {
                 </div>
             </div>
         `);
-         $(".available").append(card);
+         $(".available").prepend(card);
         //  $(".developer-container").prepend(card);
         }
     }
@@ -51,7 +143,7 @@ $.ajax({
     url: "/api/players/" + id
   })
     .then(function(response) {
-      $(".addedPlayers").append(addplayer);
+      $(".addedPlayers").prepend(addplayer);
         console.log("response", response);
        //window.location.reload();
     });
